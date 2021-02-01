14f0f719cd8fcc7120f61de211cd45a3bad541e2
Khoa Trieu <katrds1412@gmail.com>
2021-02-02 05:21:52 +0700

    Add function add to databse

--- snort.py
+++ snort.py
@@ -27,6 +27,7 @@ finally:
         cursor.close()
         connection.close()
         print ("Postgres connection is cloesed")
+    
 class snort_Function(Resource): 
     def get(self):
         name = "snort3-community.rules"
@@ -45,34 +46,69 @@ api.add_resource(snort_Function, "/rules")
     #     fh = open(name, 'a')
     
 # r_pos = 0
-# # rule = rules[r_pos]
-# # print(rules[r_pos])
-# # header = rule[:rule.find("(")].rstrip().split(" ")
-# # option = rule[rule.find("("):]
-# # rule_d = {
-# #     'Status': "",
-# #     'Action': "",
-# #     'Proto': "",
-# #     'IpSrc': "",
-# #     'PortSrc': "",
-# #     'Operation': "",
-# #     'IpDes': "",
-# #     'PortDes': "",
-# #         }
-# # h_pos = 0
-# # for x in rule_d:
-# #     rule_d[x] = header[h_pos]
-# #     h_pos += 1
+# rule = rules[r_pos]
+# print(rules[r_pos])
+# header = rule[:rule.find("(")].rstrip().split(" ")
+# option = rule[rule.find("("):]
+# rule_d = {
+#     'Status': "",
+#     'Action': "",
+#     'Proto': "",
+#     'IpSrc': "",
+#     'PortSrc': "",
+#     'Operation': "",
+#     'IpDes': "",
+#     'PortDes': "",
+#         }
+# h_pos = 0
+# for x in rule_d:
+#     rule_d[x] = header[h_pos]
+#     h_pos += 1
 # rules.pop(r_pos)
 # print(rules[0])
-# # rule_d["Status"] = ""
-# # rule_d["Action"] = ""
-# # rules[r_pos] =" " . join(str(val) for x, val in rule_d.items()) + " " + option
-# # print(rules[r_pos])
+# rule_d["Status"] = ""
+# rule_d["Action"] = ""
+# rules[r_pos] =" " . join(str(val) for x, val in rule_d.items()) + " " + option
+# print(rules[r_pos])
 # fh = open(name, 'w')
 # for rule in rules:
 #     fh.write(rule)
-# # print(option)
+# print(option)
+
+## Add Rule to addbase
+
+# try:
+#     connection = psycopg2.connect(user="postgres", 
+#                                 password="se130090", 
+#                                 host= "10.10.2.6", 
+#                                 port= "5432", 
+#                                 database="snort")
+#     cursor = connection.cursor()
+#     for line in rules:
+#         rule = line
+#         header = rule[:rule.find("(")].rstrip().split(" ")
+#         if (len(header) != 8):
+#             header.insert(0, " ")
+#         option = rule[rule.find("("):]
+#         h_pos = 0
+#         for x in rule_d:
+#             rule_d[x] = header[h_pos]
+#             h_pos += 1
+#         if (rule[0] == '#'):
+#             rule_d["Status"] = 0
+#             cursor.execute(f'''insert into snort_rule (action, protocol, ip_src, port_src, direction, ip_des, port_des, rule_option, status)
+#                             values 
+#                             ('{rule_d["Action"]}', '{rule_d["Proto"]}', '{rule_d["IpSrc"]}', '{rule_d["PortSrc"]}', '{rule_d["Operation"]}', '{rule_d["IpDes"]}', '{rule_d["PortDes"]}', '{option}', '{rule_d["Status"]}')'''
+#                             )
+#             connection.commit()
+#         else:
+#             rule_d["Status"] = 1
+#             cursor.execute(f'''insert into snort_rule (action, protocol, ip_src, port_src, direction, ip_des, port_des, rule_option, status)
+#                             values 
+#                             ('{rule_d["Action"]}', '{rule_d["Proto"]}', '{rule_d["IpSrc"]}', '{rule_d["PortSrc"]}', '{rule_d["Operation"]}', '{rule_d["IpDes"]}', '{rule_d["PortDes"]}', '{option}', '{rule_d["Status"]}')'''
+#                             )
+#             connection.commit()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
